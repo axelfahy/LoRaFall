4385866217efa4d600f89650f0117c127f43c800
Kaxl <kaxlinfo@gmail.com>
2016-12-20 22:24:34 +0100

    feat(server): First and only route planned for now (home) is working.               It doesn't print anything but the structure works.

---
+++ server/app.js
@@ -0,0 +1,58 @@
+
+//////////////////
+// Requirements //
+//////////////////
+var express = require('express');
+var path = require('path');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+///////////
+// Setup //
+///////////
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Express setup
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Security - CORS
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'x-access-token');
+	next();
+});
+
+////////////
+// Routes //
+////////////
+
+app.use('/', routes);
+
+////////////////////
+// Error handlers //
+////////////////////
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// Production error handler
+// No stacktraces leaked to user
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: err, // Change to {} to remove stacktraces
+		title: 'error'
+	});
+});
+
+module.exports = app;
---
+++ server/config.js
@@ -0,0 +1,10 @@
+var os = require("os");
+
+var config = {
+    // App infos
+    app: {
+        name: "LoraFall Application",
+        port: 5000
+    }
+};
+module.exports = config;
